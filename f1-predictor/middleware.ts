a0bf374fe20e181dbdbe081d26290a61c0a1d367
Ralph <ralphdebono@gmail.com>
2026-05-11 22:09:19 +0200

    Allow /blog routes to be accessed publically

--- f1-predictor/middleware.ts
+++ f1-predictor/middleware.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
   "/groups",
   "/global-leaderboard",
+  "/blog(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
